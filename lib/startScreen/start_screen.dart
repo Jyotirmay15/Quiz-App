d73f76a16179225b0cd8d60c3b18bf3979c544e3
Jyotirmay15 <jyomishra99@gmail.com>
2025-04-13 00:09:41 +0530

    Fixed the home screen styling

--- lib/startScreen/start_screen.dart
+++ lib/startScreen/start_screen.dart
@@ -1,32 +1,35 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-
   const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Quiz'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Welcome to the Quiz!',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Action when button is pressed
-              },
-              child: Text('Start Quiz'),
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 200,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          SizedBox(height: 50),
+          Text(
+            'Welcome to the Quiz!',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+          SizedBox(height: 20),
+          OutlinedButton.icon(
+            onPressed: () {
+              // Action when button is pressed
+            },
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: Icon(Icons.arrow_right_alt, size: 50),
+            label: Text('Start Quiz', style: TextStyle(fontSize: 24)),
+          ),
+        ],
       ),
     );
   }
